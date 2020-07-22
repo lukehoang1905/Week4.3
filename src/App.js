6438992e47758e9aeb7ce7e37addf1d017b67f09
Luke Hoang <lukehoang195@gmail.com>
2020-07-22 16:58:50 +0700

    first draft

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+
+import React, { Component } from "react";
+
+import "bootstrap/dist/css/bootstrap.min.css"
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+// const KEY = process.env.REACT_API_KEY
+//   console.log(KEY)
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    // Don't call this.setState() here!
+    this.state = { weather:null};
+    this.state = {
+      isLoading: true
+    };
+  }
+  componentDidMount(){
+    this.getLocation()
+  }  
+  
+  async getWeather(lon, lat){
+    const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=3a8897c127708dd906da32088a599881`;
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data)
+    this.setState({
+      locationName: data.name,
+      temp : data.main.temp,
+      weather: data.weather[0].description,
+      icon: data.weather[0].icon
+    })
+    }
+ 
+  getLocation  = () => {
+   navigator.geolocation.getCurrentPosition((post) => {
+    this.getWeather(post.coords.longitude, post.coords.latitude)
+    })
+  }
+  render() {
+    return (
+      <div className="container-fluid text-white my-auto">
+        <div className="container mx-auto my-4 py-4">
+          <div className="row justify-content-center text-center">
+            <h1 className="col-12 display-4 my-2 py-3 text-success">
+              Awesome Weather App
+            </h1>
+    <h2 className="col-12">{this.state.locationName}</h2>
+    <h3 className="col-12 text-danger">{this.state.temp}°F or {(this.state.temp-32)*5/9}°C</h3>
+            <h3 className="col-12">{this.state.weather}</h3>
+            <div className="weather-icon"><img src={`icons/09d.png`} alt="icon"/></div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
